375a5b3738aff186338eeafa6a1815f1d46f6c27
luisgustavo <luis.gustav0@live.com>
2019-06-18 21:04:16 -0300

    primeiro commit

--- primeiro-projeto/src/index.js
+++ primeiro-projeto/src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 
 const element = document.getElementById('root');
 
-ReactDOM.render(<h1> Ola Mundo </h1>, element);
+ReactDOM.render(
+    <h1> Ola Mundo </h1>, element
+);
+
+const lista = document.getElementById('ul');
+
+let item = document.createElement('li');
+let texto = document.createTextNode('1) Luis');
+
+item.appendChild(texto);
+lista.appendChild(item);
